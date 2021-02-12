bb5738ec978804fd9bc6a9c5a5278d092f6d4b92
Marcelo Amuchastegui <subizbrasil@gmail.com>
2021-02-12 10:33:13 -0300

    responsividade

--- pokedex/src/Styled/Styled.js
+++ pokedex/src/Styled/Styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-
 export const MainContainer = styled.div`
 display:grid;
 grid-template-rows:10% 88% 2%;
@@ -8,14 +7,12 @@ width:100%;
 height:100vh;
 background-color:#d4d4d3;
 `
-
 export const HeaderStyle = styled.div`
 grid-row:1;
 background-color:#555251;
 width:100%;
 height:100%;
 `
-
 export const HomeStyle = styled.div`
 display:grid;
 grid-template-columns: repeat(4,20%);
@@ -28,8 +25,13 @@ justify-items:center;
 justify-content:center;
 overflow:scroll;
 overflow-x:hidden;
+@media (min-width:500px) and (max-width: 800px) {
+  grid-template-columns: repeat(3,30%);
+  };
+@media (max-width: 499px) {
+  grid-template-columns: repeat(2,50%);
+  };
 `
-
 export const PokeCard = styled.div`
 display:flex;
 flex-wrap:wrap;
@@ -42,12 +44,12 @@ border-top-right-radius:10px;
 border-top-left-radius:10px;
 opacity: 0.7;
 `
-
 export const PokePhoto = styled.img`
 display:flex;
 flex-direction:row;
-width: 100%;
-height: 100%;
+margin:auto;
+width: 90%;
+height: 85%;
 `
 ///POKEDETAILS///
 export const PokeDetailsGrid = styled.div`
@@ -98,7 +100,6 @@ grid-row:2/4;
 margin:auto;
 background-color:#f6f6f6;
 `
-
 export const PokeType = styled.div`
 display:grid;
 width:95%;
@@ -124,7 +125,6 @@ padding-bottom:4%;
   transform: scale(1.1);
 }
 `;
-
 export const Button = styled.button`
   font-size: 1em;
   margin: 0.7em 12em 1em 15em;
@@ -139,12 +139,12 @@ export const Button = styled.button`
       main: "tomato"
     }
 }
-
-
 export const ButtonSmall = styled.button`
-  font-size: 1em;
+  font-size: 0.8em;
   margin: 0.1em 0.1em;
   border-radius: 3px;
+  margin:auto;
+  max-width:48%;
   color:#fff;
   background-color: ${props => props.theme.main};
   border: 2.5px solid ${props => props.theme.main};
@@ -154,14 +154,15 @@ export const ButtonSmall = styled.button`
       main: "tomato"
     }
 }
-
 export const LetterP = styled.p`
 color:#000;
 text-align:center;
 font-weight:500;
 text-transform:capitalize;
-margin-bottom:-0.5em;
-font-size:1.5vw;
+margin:auto;
+margin-bottom:-1em;
+font-size:1.4vw;
 background-size: 14px;
 line-height:50%;
+font-size:1em;
 `;
